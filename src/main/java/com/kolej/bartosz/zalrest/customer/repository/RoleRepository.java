46382e3ca6285b23d00f8272a48e8e89260fce8b
Bartosz Kolej <b.kolej@gmail.com>
2023-03-23 20:54:32 +0100

    Add repo adnotation

--- src/main/java/com/kolej/bartosz/zalrest/customer/repository/RoleRepository.java
+++ src/main/java/com/kolej/bartosz/zalrest/customer/repository/RoleRepository.java
@@ -2,10 +2,12 @@ package com.kolej.bartosz.zalrest.customer.repository;
 
 import com.kolej.bartosz.zalrest.customer.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Optional<Role> getRoleByName(String name);
 }
--- src/main/java/com/kolej/bartosz/zalrest/customer/repository/UserRepository.java
+++ src/main/java/com/kolej/bartosz/zalrest/customer/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.kolej.bartosz.zalrest.customer.repository;
 
 import com.kolej.bartosz.zalrest.customer.model.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<CustomUser, UUID> {
     Optional<CustomUser> getMyUserByUsername(String username);
     void deleteByUsername(String username);
